8561755bd8fa26c3f506329ed8cc1da8551f66fe
lewiswon <lewis.won@gmail.com>
2023-03-14 00:11:55 +0800

    exercise 1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -11,17 +11,48 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const addGood = () => {
+    const updatedGood = good +1 
+    const total = updatedGood + neutral + bad
+    setGood(updatedGood)
+    setAll(total)
+    setAverage((updatedGood - bad)/total)
+    setPositive((updatedGood)/total*100)
+  }
+  const addNeutral = () => {
+    const updatedNeutral = neutral +1 
+    const total = good + updatedNeutral + bad
+    setNeutral(updatedNeutral)
+    setAll(total)
+    setAverage((good - bad)/total)
+    setPositive((good)/total*100)
+  }
+  const addBad = () => {
+    const updatedBad = bad +1 
+    const total = good + neutral + updatedBad
+    setBad(updatedBad)
+    setAll(total)
+    setAverage((good - updatedBad)/total)
+    setPositive((good)/total*100)
+  }
 
   return (
     <>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good+1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral+1)} text="neutral" />
-      <Button handleClick={() => setBad(bad+1)} text="bad" />
+      <Button handleClick={addGood} text="good" />
+      <Button handleClick={addNeutral} text="neutral" />
+      <Button handleClick={addBad} text="bad" />
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
 
     </>
   )
